20badaf4d59669815e218a2c7b4b076b7b57b809
Victor H Montanher <140670609+VictorMHB@users.noreply.github.com>
2026-08-14 15:24:41 -0300

    fix back

--- src/pages/funcionario/FormFuncionario.jsx
+++ src/pages/funcionario/FormFuncionario.jsx
@@ -105,7 +105,7 @@ function FormFuncionario() {
         toast.success("Funcionário atualizado com sucesso!");
       } else {
         const res = await funcionarioService.criar(dadosLimpos);
-        // toast.success(res.data);
+        toast.success(res.data);
         toast.success("Funcionário cadastrado com sucesso! A senha temporária foi enviada no e-mail cadastrado.");
       }
       navigate("/funcionarios");
